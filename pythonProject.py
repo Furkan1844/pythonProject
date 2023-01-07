a792bd70900cb5689fad72575e26a817f4581beb
Furkan1844 <121129602+Furkan1844@users.noreply.github.com>
2023-01-07 18:23:12 +0300

    Update pythonProject.py

--- pythonProject.py
+++ pythonProject.py
@@ -1,11 +1,32 @@
-def getInput(): # Displays options and gets an input
+import os
+
+def itemDict(): # Makes a dict of items
+    pass
+def checkFolderExist(): # Checks if the file exist in the current path through os module
+    getCWD = os.getcwd()
+    fileControl = os.listdir()
+    fileList = []
+    for file in fileControl:
+        fileList.append(file)
+        if file == 'items.json':
+            return getInput()
+    else:
+        yesno = input("File can not found!\nDo you want to create one?(y/n) : ")
+        yesno = ('"' + yesno + '"')
+    if yesno == "y":
+        os.mkdir(getCWD + "\items.json")
+        print("ok")
+
+
+
+def getInput(): # Displays options
     print("====================\n Inventory Options\n====================\n")
     options = ("1) Add an Item\n2) Delete an Item\n3) Update Item Quantity\n4) Change Item Name\n5) Exit\n")
     print(options)
-    choice = int(input("Chose an option:"))
+    choice = int(input("Choose an option : "))
     driveOptions(choice)
 
-def driveOptions(choice): # Forwards input to function that user want
+def driveOptions(choice): # Forwards input to function that user chose
     if choice == 1:
         addItem()
     if choice == 2:
@@ -16,15 +37,18 @@ def driveOptions(choice): # Forwards input to function that user want
         changeName()
     if choice == 5:
         pass
-    
-def addItem():
+
+def addItem(): 
     pass
 
-def deleteItem():
+def addQuantity(): # When user add an item, this func will ask the quantity
     pass
 
-def updateQuantity():
+def deleteItem(): # Delete item by id
     pass
 
-def changeName():
+def updateQuantity(): # Changes the quantity by id of the item
+    pass
+
+def changeName(): # Changes the name by id of the item
     pass
