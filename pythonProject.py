44e3d26043499a7dac853300723fd1570da7cd34
Furkan1844 <121129602+Furkan1844@users.noreply.github.com>
2023-01-07 23:30:47 +0300

    Update pythonProject.py

--- pythonProject.py
+++ pythonProject.py
@@ -1,19 +1,18 @@
 import os
 
-def itemDict(): # Makes a dict of items
-    pass
+
 def checkFolderExist(): # Checks if the file exist in the current path through os module
     fileControl = os.listdir()
-    filename = "items.json"
+    filename = "items.txt"
     for file in fileControl:
-        if file == "items.json":
+        if file == "items.txt":
             return getInput()
     else:
         yesno = input("File can not found!\nDo you want to create one?(y/n) : ")
         if yesno == "y":
-            file = open(filename, "w")
+            file = open(filename, "x")
             return getInput()
-
+    
 def getInput(): # Displays options
     print("====================\n Inventory Options\n====================\n")
     options = ("1) Add an Item\n2) Delete an Item\n3) Update Item Quantity\n4) Change Item Name\n5) Exit\n")
@@ -33,10 +32,25 @@ def driveOptions(choice): # Forwards input to function that user chose
     if choice == 5:
         pass
 
-def addItem(): 
-    pass
+def addItem():
+    print("====================\n Adding Item\n====================\n")
+    item = {
+        "id" : "",
+        "name" : None,
+        "quantity" : None,
+        "date" : ""
+    }
+    item["name"] = input("Enter item's name : ")
+    item["quantity"] = input("Enter item's quantity : ")
+    w = open("items.txt", "a")
+    w.write("\n{\n")
+    for key,value in item.items():
+        w = open("items.txt", "a")
+        w.write(key + " : " + value + "\n")
+    w.write("}")
+    w.close
 
-def addQuantity(): # When user add an item, this func will ask the quantity
+def writeItemToFile():
     pass
 
 def deleteItem(): # Delete item by id
@@ -47,3 +61,5 @@ def updateQuantity(): # Changes the quantity by id of the item
 
 def changeName(): # Changes the name by id of the item
     pass
+
+checkFolderExist()
